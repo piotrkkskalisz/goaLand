566d0eb723329dddcdc43320388e283e3f6cdce0
piotrkkskalisz <pawlaczyk.kopernik@gmail.com>
2026-07-26 18:41:07 +0200

    feature(cmd): add API entry point

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+
+	"backend/internal/database"
+	"backend/internal/server"
+)
+
 func main() {
 
+	db, err := database.NewClientFromEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	server := server.NewServer(db)
+
+	log.Println("Starting server on :8080")
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
