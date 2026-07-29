f279b44ecf86132f980f8ec114d1b39d1bc2103a
piotrkkskalisz <pawlaczyk.kopernik@gmail.com>
2026-07-29 16:50:11 +0200

    feature(handler): add transform layer for handlers

---
+++ backend/internal/transform/tranform.go
@@ -0,0 +1,52 @@
+package transform
+
+import (
+	"backend/internal/database"
+	"backend/internal/utils"
+)
+
+type CompetitionEdition struct {
+	CompetitionID int `json:"competitionId"`
+
+	Name            string `json:"name"`
+	Code            string `json:"code"`
+	CompetitionType string `json:"competitionType"`
+
+	StartYear int    `json:"startYear"`
+	Status    string `json:"status"`
+}
+
+func CreateCompetitionEdition(competition database.Competition, edition database.Edition) CompetitionEdition {
+	return CompetitionEdition{
+		CompetitionID: competition.CompetitionID,
+
+		Name:            competition.Name,
+		Code:            competition.Code,
+		CompetitionType: competition.CompetitionType,
+
+		StartYear: edition.StartYear,
+		Status:    edition.Status,
+	}
+}
+
+func getAcitve(competition database.Competition) (CompetitionEdition, bool) {
+	for _, edition := range competition.Editions {
+		if edition.Status == utils.EditionActive {
+			return CreateCompetitionEdition(competition, edition), true
+		}
+	}
+
+	return CompetitionEdition{}, false
+}
+
+func GetCompetitionEdition(competitions []database.Competition) []CompetitionEdition {
+	var response []CompetitionEdition
+
+	for _, competition := range competitions {
+		if competitionEdition, ok := getAcitve(competition); ok {
+			response = append(response, competitionEdition)
+		}
+	}
+
+	return response
+}
