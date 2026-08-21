8bde80d795ca767609a3c75cefdd298756784844
piotrkkskalisz <pawlaczyk.kopernik@gmail.com>
2026-08-21 12:51:01 +0200

    feature(components): link header logo to home page

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,10 +1,16 @@
 import { Search } from "lucide-react";
+import { Link } from "react-router";
 
 export function Header() {
   return (
     <header className="flex h-[109px] w-full items-center justify-center gap-[50px] rounded-lg bg-header px-[77px] py-[25px]">
       <h1 className="w-[760px] text-center text-heading-bold">
-        Goaland
+        <Link
+          className="inline-block rounded-sm px-[10px] py-[5px]"
+          to="/"
+        >
+          Goaland
+        </Link>
       </h1>
 
       <label className="flex h-[60px] w-[300px] items-center gap-[10px] rounded-full bg-search px-[20px]">
