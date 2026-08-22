f04d3003c68f036dbe52191a57b1454cd0ba34c6
piotrkkskalisz <pawlaczyk.kopernik@gmail.com>
2026-08-22 16:31:28 +0200

    feature(utils): add match status groups

--- backend/internal/utils/statuses.go
+++ backend/internal/utils/statuses.go
@@ -8,6 +8,24 @@ const (
 	EditionFinished = "FINISHED"
 )
 
+var UpcomingMatchStatuses = []string{
+	"SCHEDULED",
+	"TIMED",
+}
+
+var LiveMatchStatuses = []string{
+	"LIVE",
+	"IN_PLAY",
+	"PAUSED",
+	"EXTRA_TIME",
+	"PENALTY_SHOOTOUT",
+}
+
+var FinishedMatchStatuses = []string{
+	"FINISHED",
+	"AWARDED",
+}
+
 func EditionStatus(startDate time.Time, endDate time.Time) string {
 	now := time.Now()
 	if now.Before(startDate) {
