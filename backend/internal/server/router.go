dd21ab82a5bbe70c9b7ee9682bc14a893c5311b2
piotrkkskalisz <pawlaczyk.kopernik@gmail.com>
2026-08-27 13:17:14 +0200

    enable frontend api access (#50)

--- backend/internal/server/router.go
+++ backend/internal/server/router.go
@@ -8,10 +8,17 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func NewRouter(c *database.Client) http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://localhost:5173"},
+		AllowedMethods: []string{"GET", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Content-Type"},
+	}))
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
